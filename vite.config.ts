fda81e12e7262b6526d9175e598750012070fb3c
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-18 15:40:01 +0200

    add deployment to gh pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
+  server: {
+    port: 4200,
+  },
 })
+
