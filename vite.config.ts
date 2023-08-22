9870138d5f591d9873749e082a71fb102e19ff80
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-22 15:32:46 +0200

    reinstall eslint-plugin-import and fix aliases

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
-import react from '@vitejs/plugin-react-swc'
 import { URL, fileURLToPath } from 'node:url'
 import { defineConfig } from 'vite'
 import eslint from 'vite-plugin-eslint'
 import svgr from 'vite-plugin-svgr'
+
+import react from '@vitejs/plugin-react-swc'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,12 @@ export default defineConfig({
   },
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url)),
+      // repeats in tsconfig.ts, .eslintrc.cjs, vite.config.ts
+      '@assets': fileURLToPath(new URL('./src/assets', import.meta.url)),
+      '@components': fileURLToPath(new URL('./src/components', import.meta.url)),
+      '@hooks': fileURLToPath(new URL('./src/hooks', import.meta.url)),
+      '@models': fileURLToPath(new URL('./src/models', import.meta.url)),
+      '@utils': fileURLToPath(new URL('./src/utils', import.meta.url)),
     },
   },
 })
