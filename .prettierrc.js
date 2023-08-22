19f794ef2d9fe87b1d10ccb329b73dd3ac52b302
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-22 17:09:20 +0200

    change prettier from json to js

---
+++ .prettierrc.js
@@ -0,0 +1,15 @@
+/** @type {import("prettier").Config} */
+const config = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  trailingComma: 'all',
+  plugins: ['prettier-plugin-organize-imports', '@trivago/prettier-plugin-sort-imports'],
+  importOrder: ['^@', '^[./]'],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+}
+
+export default config
