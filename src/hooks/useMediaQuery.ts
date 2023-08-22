a2b2f6be05ba4e3a677fca2c8c9574d9c419ea21
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-22 17:30:58 +0200

    fix useMediaQuery useEffect deps

--- src/hooks/useMediaQuery.ts
+++ src/hooks/useMediaQuery.ts
@@ -16,7 +16,7 @@ export function useMediaQuery(query: string): boolean {
       mediaQuery.addEventListener('change', handler)
       return () => mediaQuery.removeEventListener('change', handler)
     },
-    [], // Empty array ensures effect is only run on mount and unmount
+    [query], // Empty array ensures effect is only run on mount and unmount
   )
   return matches
 }
