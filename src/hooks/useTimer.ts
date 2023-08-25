38eb32d9ad4419c092aa769deac4d96cd3c1a008
Bohdan Vinter <bogdan.chornocondratenko@gmail.com>
2023-08-25 16:50:56 +0200

    add audio alarm on time out

--- src/hooks/useTimer.ts
+++ src/hooks/useTimer.ts
@@ -1,5 +1,8 @@
+import alarm from '@/assets/alarm-digital.mp3'
 import { WorkerInterval } from '@/utils'
 import { useCallback, useEffect, useLayoutEffect, useReducer, useState } from 'react'
+
+const alarmAudio = new Audio(alarm)
 
 const DEV_SPEED_UP = import.meta.env.DEV ? 1000 : 1
 const STEP_MS = 10
@@ -46,6 +49,7 @@ export function useTimer(totalMs: number) {
         const newMsLeft = Math.max(0, prevMsLeft - STEP_MS * DEV_SPEED_UP)
         if (newMsLeft === 0) {
           setIsRunning(false)
+          alarmAudio.play()
         }
         return newMsLeft
       }
